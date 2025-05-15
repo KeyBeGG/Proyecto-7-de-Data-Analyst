afc5713c5a0287e4344ef2447390c942d22937d3
KeyBeGG <kbgg3108@gmail.com>
2025-05-15 02:01:52 -0500

    Cambios V2

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') 
+
+#Titulo de la aplicacion 
+st.title("Mi primera app con Streamlit")
+#Agregar un texto
+st.write("Es una pagina web simple para aprobar el proyecto")
+st.write("Usa el siguiente Boton para crear una histograma")
+
+#Agrega un boton para crear un histograma simple
+if st.button("Ver histograma"):
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    #Crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+#Agrega un boton para crear un gráfico intereactivo
+if st.button("Ver gráfico"):
+    st.write('Creación de un gráfico para el conjunto de datos de anuncios de venta de coches')
+    #Mostrar un gráfico Plotly interactivo
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
--- notebooks/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
-car_data = pd.read_csv('vehicles_us.csv') 
